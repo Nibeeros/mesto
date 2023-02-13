e5603b641af46474c7c7b86c7f1bc1614ae650fe
nibeeros <Nibeeros@gmail.com>
2023-02-13 14:41:53 +0300

    работа над ошибками

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,13 @@
 let popup = document.querySelector('.popup');
 let popupContainer = popup.querySelector('.popup__container');
-let saveButton = popupContainer.querySelector('.popup__button-save');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = popupContainer.querySelector('.popup__button-close');
 let profileInfo = document.querySelector('.profile__info');
 let profileName = profileInfo.querySelector('.profile__name');
 let profileJob = profileInfo.querySelector('.profile__job');
-let inputName = popup.querySelector('.popup__profile-name');
-let inputJob = popup.querySelector('.popup__profile-job');
+let inputName = popup.querySelector('.popup__input_profile-name');
+let inputJob = popup.querySelector('.popup__input_profile-job');
 let popupForm = popupContainer.querySelector('.popup__form');
-inputName.value = profileName.textContent;
-inputJob.value = profileJob.textContent;
 
 function closePopup() {
   popup.classList.remove('popup_opened');
@@ -18,9 +15,11 @@ function closePopup() {
 
 function openPopup() {
   popup.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
 };
 
-function handleFormSubmit (evt) {
+function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
