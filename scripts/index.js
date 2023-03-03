6b1edbd38cc0244c1c40f0fcc7c67cd640d9a6ac
nibeeros <Nibeeros@gmail.com>
2023-03-03 17:24:27 +0300

    сделать 2 попапа и добавить к ним функциональность

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,113 @@
-let popup = document.querySelector('.popup');
-let popupContainer = popup.querySelector('.popup__container');
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = popupContainer.querySelector('.popup__button-close');
-let profileInfo = document.querySelector('.profile__info');
-let profileName = profileInfo.querySelector('.profile__name');
-let profileJob = profileInfo.querySelector('.profile__job');
-let inputName = popup.querySelector('.popup__input_info_name');
-let inputJob = popup.querySelector('.popup__input_info_job');
-let popupForm = popupContainer.querySelector('.popup__form');
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const popupEditButtonClose = document.querySelector('.popup__edit-button-close');
+const popupAddButtonClose = document.querySelector('.popup__add-button-close');
+const popupImageButtonClose = document.querySelector('.popup__image-button-close');
+const editPopup = document.querySelector('.popup_edit-profile');
+const addPopup = document.querySelector('.popup_add-card');
+const imagePopup = document.querySelector('.popup_image');
+const linkImage = document.querySelector('.popup__card-image');
+const nameImage = document.querySelector('.popup__caption');
+const popupEditNameInput = document.querySelector('.popup__input_info_name');
+const popupEditJobInput = document.querySelector('.popup__input_info_job');
+const popupAddCardName = document.querySelector('.popup__card-name');
+const popupAddCardLink = document.querySelector('.popup__card-link');
+const editform = document.querySelector('.popup__edit-form');
+const addform = document.querySelector('.popup__add-form');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const cardsList = document.querySelector('.cards');
+const cardTemplate = document.querySelector('.cards__template').content;
+const cardTitle = document.querySelector('.cards__title')
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
-};
-
-function openPopup() {
+function openPopup(popup) {
   popup.classList.add('popup_opened');
-  inputName.value = profileName.textContent;
-  inputJob.value = profileJob.textContent;
-};
+}
 
-function handleFormSubmit(evt) {
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+function defaultProfileData() {
+  popupEditNameInput.value = profileName.textContent;
+  popupEditJobInput.value = profileJob.textContent;
+}
+
+function editFormSubmitHandler(evt) {
   evt.preventDefault();
-  profileName.textContent = inputName.value;
-  profileJob.textContent = inputJob.value;
-  closePopup();
-};
+  profileName.textContent = popupEditNameInput.value;
+  profileJob.textContent = popupEditJobInput.value;
+  closePopup(editPopup);
+}
 
-popupForm.addEventListener('submit', handleFormSubmit);
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+function addFormSubmitHandler(evt) {
+  evt.preventDefault();
+  addCard(cardsList, createCard(popupAddCardName.value, popupAddCardLink.value));
+  closePopup(addPopup);
+  evt.target.reset();
+}
 
+function addCard(container, element) {
+  container.prepend(element);
+}
+
+function addInfoImage(name, link) {
+  nameImage.textContent = name;
+  nameImage.alt = name;
+  linkImage.src = link;
+}
+
+function createCard(name, link) {
+  const card = cardTemplate.cloneNode(true);
+  const cardImage = card.querySelector('.cards__image');
+  const cardTitle = card.querySelector('.cards__title');
+  const cardLike = card.querySelector('.cards__like');
+  const cardBasket = card.querySelector('.cards__basket');
+  cardTitle.textContent = name;
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardBasket.addEventListener('click', deleteCard);
+  cardLike.addEventListener('click', activateLike);
+  cardImage.addEventListener('click', () => {
+    addInfoImage(name, link),
+      openPopup(imagePopup)
+  });
+  return card;
+}
+
+function activateLike(event) {
+  event.target.classList.toggle('cards__like_active');
+}
+
+function deleteCard(event) {
+  event.target.closest('.cards__item').remove();
+}
+
+editButton.addEventListener('click', () => {
+  openPopup(editPopup),
+    defaultProfileData()
+});
+
+addButton.addEventListener('click', () => {
+  openPopup(addPopup)
+});
+
+popupEditButtonClose.addEventListener('click', () => {
+  closePopup(editPopup)
+})
+
+popupAddButtonClose.addEventListener('click', () => {
+  closePopup(addPopup)
+})
+
+popupImageButtonClose.addEventListener('click', () => {
+  closePopup(imagePopup)
+})
+
+editform.addEventListener('submit', editFormSubmitHandler);
+addform.addEventListener('submit', addFormSubmitHandler);
+
+
+initialCards.forEach((data) => {
+  addCard(cardsList, createCard(data.name, data.link))
+});
---
+++ scripts/initial-cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Мост',
+    link: 'https://images.unsplash.com/photo-1677662375194-e5157a8e09b9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    name: 'Туманные горы',
+    link: 'https://images.unsplash.com/photo-1677442638168-800f372ee67d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'
+  },
+  {
+    name: 'Улица в китайском квартале',
+    link: 'https://images.unsplash.com/photo-1677629322314-9c404559f7f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80'
+  },
+  {
+    name: 'Река',
+    link: 'https://images.unsplash.com/photo-1677596791149-dfde45568d6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    name: 'Место для двоих',
+    link: 'https://images.unsplash.com/photo-1677425903803-986327eb8979?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    name: 'Горная дорога',
+    link: 'https://images.unsplash.com/photo-1677616842809-0a5297aced15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+  }
+];
