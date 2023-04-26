7dba367acd81f261d1cf3b7788efd38e29a2706b
nibeeros <nibeeros@gmail.com>
2023-04-26 07:48:33 +0300

    ПР 8 добавлены новые классы. Подключён вебпак. Создана новая структура

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,42 +1,28 @@
-const popupImgAdd = document.querySelector(".popup__images");
-const popupImgTitle = document.querySelector(".popup__images-title");
-
-import { openPopup } from "./index.js";
-import { popupImg } from "./index.js";
 export default class Card {
-  constructor(initialCards, templateSelector) {
-    this._name = initialCards.name;
-    this._link = initialCards.link;
+  constructor({ name, link }, templateSelector, handleCardClick) {
+    this._name = name;
+    this._link = link;
+    this._handleCardClick = handleCardClick;
     this._templateSelector = templateSelector;
     this._element = this._getTemplate();
     this._cardImage = this._element.querySelector(".element__image");
     this._likeButton = this._element.querySelector(".element__like");
-    this._deleteBusket = this._element.querySelector(".element__del")
   }
 
   _getTemplate() {
-    // забираем разметку из HTML и клонируем элемент
     const cardTemplate = document
       .querySelector(this._templateSelector)
       .content.querySelector(".element")
       .cloneNode(true);
-
-    // вернём DOM-элемент карточки
     return cardTemplate;
   }
 
   generateCard() {
-    // Запишем разметку в приватное поле _element.
-    // Так у других элементов появится доступ к ней.
-
     this._setEventListeners();
-
-    // Добавим данные
     this._cardImage.src = this._link;
     this._cardImage.alt = this._name;
     this._element.querySelector(".element__title").textContent = this._name;
 
-    // Вернём элемент наружу
     return this._element;
   }
 
@@ -52,25 +38,22 @@ export default class Card {
     });
 
     //слушатель удаления карточки
-    this._deleteBusket.addEventListener("click", () => {
-      this._handleDeleteClick();
+    this._element
+      .querySelector(".element__del")
+      .addEventListener("click", () => {
+        this._element.remove();
       });
   }
 
-  _handleDeleteClick() {
-    this._element.remove();
-    this._element = null;
-  }
+  //переключатель лайка
 
   _handleLikeClick() {
     this._likeButton.classList.toggle("element__like_active");
   }
 
-  _handleOpenPopup() {
-    openPopup(popupImg);
+  //открытие попапа с картинкой по клику карточки
 
-    popupImgAdd.src = this._link;
-    popupImgAdd.alt = this._name;
-    popupImgTitle.textContent = this._name;
+  _handleOpenPopup() {
+    this._handleCardClick(this._link, this._name);
   }
 }
